3bcba9d1a7c145512987b9d15904e880f79d07b2
Marius Banea <marius@w-d-s.co.uk>
2020-03-09 02:22:02 +0000

    Initial commit

---
+++ src/folders/folders-service.js
@@ -0,0 +1,30 @@
+const FoldersService = {
+    getAllFolders(knex) {
+        // console.log(knex)
+        return knex.select('*').from('noteful_folders')
+    },
+    insertFolder(knex, newFolder) {
+        return knex
+            .insert(newFolder)
+            .into('noteful_folders')
+            .returning('*')
+            .then(rows => {
+                return rows[0]
+            })
+    },
+    deleteFolder(knex, id) {
+        return knex('noteful_folders')
+            .where({ id })
+            .delete()
+    },
+    getById(knex, id) {
+        return knex.from('noteful_folders').select('*').where('id', id).first()
+    },
+    // updateFolder(knex, id, newFolderFields) {
+    //     return knex('noteful_folders')
+    //         .where({ id })
+    //         .update(newFolderFields)
+    // },
+}
+
+module.exports = FoldersService
---
+++ src/notes/notes-service.js
@@ -0,0 +1,30 @@
+const NotesService = {
+    getAllNotes(knex) {
+        // console.log(knex)
+        return knex.select('*').from('noteful_notes')
+    },
+    insertNote(knex, newNote) {
+        return knex
+            .insert(newNote)
+            .into('noteful_notes')
+            .returning('*')
+            .then(rows => {
+                return rows[0]
+            })
+    },
+    deleteNote(knex, id) {
+        return knex('noteful_notes')
+            .where({ id })
+            .delete()
+    },
+    getById(knex, id) {
+        return knex.from('noteful_notes').select('*').where('id', id).first()
+    },
+    // updateNote(knex, id, newNoteFields) {
+    //     return knex('noteful_notes')
+    //         .where({ id })
+    //         .update(newNoteFields)
+    // },
+}
+
+module.exports = NotesService
